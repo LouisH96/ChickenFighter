19a470353562ca728f930326f51b2e191dc5baa2
Louis Herman <louisherman@live.be>
2020-10-29 00:16:37 +0100

    split playermovement behavior

---
+++ Assets/Characters/Farmer/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //---Components---
+    private MovementBehavior _movementBehavior = null;
+
+    void Awake()
+    {
+        _movementBehavior = GetComponent<MovementBehavior>();
+    }
+
+    void Update()
+    {
+        if (_movementBehavior == null)
+            return;
+
+        //set inputs
+        _movementBehavior.ForwardMovementRatio = Input.GetAxis("MovementVertical");
+        _movementBehavior.RotationRatio = Input.GetAxis("MovementHorizontal");
+        _movementBehavior.ShouldJump = Input.GetAxis("Jump") > 0.0f;
+    }
+}
--- Assets/Characters/MovementBehavior.cs
+++ Assets/Characters/MovementBehavior.cs
@@ -7,16 +7,69 @@ public class MovementBehavior : MonoBehaviour
     //---Components---
     private CharacterController _characterController = null;
 
+    //Stats
+    [SerializeField] private float _forwardAcceleration = 6.0f;
+    [SerializeField] private float _maxFowardVelocity = 3.0f;
+    [SerializeField] private float _rotateSpeed = 60.0f;
+    [SerializeField] private float _jumpForce = 5.0f;
+    private float _gravity = -9.81f;
 
-    // Start is called before the first frame update
-    void Start()
+    //public
+    private float _acceleration = 0.0f;
+    public float ForwardMovementRatio { set { _acceleration = value * _forwardAcceleration; } }
+
+    private float _rotation = 0.0f;
+    public float RotationRatio { set { _rotation = value * _rotateSpeed; } }
+
+    private bool _shouldJump = false;
+    public bool ShouldJump { set { _shouldJump = value; } }
+
+    //variables
+    private Vector3 _currentVelocity = Vector3.zero;
+
+
+    void Awake()
     {
-        
+        _characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        HandleRotation();
+        HandleMovement();
+    }
+
+    private void HandleRotation()
+    {
+        transform.Rotate(0, _rotation * Time.fixedDeltaTime, 0);
+    }
+
+    private void HandleMovement()
+    {
+        if (!_characterController)
+            return;
+
+        //forward movement
+        if (_acceleration != 0.0f)
+        {
+            _currentVelocity.z += _acceleration * Time.fixedDeltaTime;
+            _currentVelocity.z = Mathf.Clamp(_currentVelocity.z, -_maxFowardVelocity, _maxFowardVelocity);
+        }
+        else
+            _currentVelocity.z = Mathf.MoveTowards(_currentVelocity.z, 0.0f, _forwardAcceleration * Time.fixedDeltaTime);
+
+        //jump
+        if (_shouldJump && _characterController.isGrounded)
+            _currentVelocity.y += _jumpForce;
+
+        //gravity
+        _currentVelocity.y += _gravity * Time.fixedDeltaTime;
+
+        //do movement
+        _characterController.Move(transform.TransformDirection(_currentVelocity) * Time.fixedDeltaTime);
+
+        //ground collision
+        if (_characterController.isGrounded)
+            _currentVelocity.y = 0.0f;
     }
 }

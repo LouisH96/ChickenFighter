59ba6d74ba0503445cec3b08aecb36ff03125d6f
Louis Herman <louisherman@live.be>
2020-10-29 01:18:02 +0100

    crappy new wander for chickens

--- Assets/Scripts/WanderBehavior.cs
+++ Assets/Scripts/WanderBehavior.cs
@@ -6,18 +6,36 @@ using UnityEngine;
 
 public class WanderBehavior : MonoBehaviour
 {
-    private PositionMovementBehavior _movementBehavior;
+    //---Components---
+    private MovementBehavior _movementBehavior = null;
+
+    //---Stats---
+    [SerializeField] private float _maxRatioChange = 0.5f;
+    private float _currentRatio = 0.0f;
+
+
+    private void Awake()
+    {
+        _movementBehavior = GetComponent<MovementBehavior>();
+    }
+
+    //private void Update()
+    //{
+    //    if (!_movementBehavior)
+    //        return;
+
+    //    _currentRatio += Random.Range(-_maxRatioChange, _maxRatioChange);
+    //    _currentRatio = Mathf.Clamp(_currentRatio, -1.0f, 1.0f);
+
+    //    _movementBehavior.RotationRatio = _currentRatio;
+    //    _movementBehavior.ForwardMovementRatio = 1.0f;
+    //}
 
     //wander based on circle in front of character
     [SerializeField] private float _offset = 1.0f;
     [SerializeField] private float _radius = 1.0f;
     [SerializeField] private float _maxChange = MyUtils.ToRadians(10.0f); //in radians
     private float _currentAngle = 0.0f; //in radians
-
-    private void Awake()
-    {
-        _movementBehavior = GetComponent<PositionMovementBehavior>();
-    }
 
     void Update()
     {
@@ -26,29 +44,56 @@ public class WanderBehavior : MonoBehaviour
 
         ChangeAngle();
 
-        Vector3 circlePos = transform.position + transform.forward * _offset;
-        Vector3 wanderTarget = circlePos + MyUtils.DegreesToVectorXZ(_currentAngle) * _radius;
-        
-        _movementBehavior.Destination = wanderTarget;
+        //  Vector3 circlePos = transform.position + transform.forward * _offset;
+        //Vector3 wanderTarget = circlePos + MyUtils.DegreesToVectorXZ(_currentAngle) * _radius;
+
+        Vector3 t = MyUtils.DegreesToVectorXZ(_currentAngle);
+
+        if ((t).x > 0.0f)
+            _movementBehavior.RotationRatio = 1.0f;
+        else
+            _movementBehavior.RotationRatio = -1.0f;
+
+        _movementBehavior.ForwardMovementRatio = 1.0f;
+        //_movementBehavior.Destination = wanderTarget;
     }
-
-    //private void OnDrawGizmos()
-    //{
-    //    if (!GizmoManager.Instance.DrawWanderCircle)
-    //        return;
-
-    //    Vector3 circlePos = transform.position + transform.forward * _offset;
-    //    Vector3 wanderTarget = circlePos + MyUtils.DegreesToVectorXZ(_currentAngle) * _radius;
-
-    //    Handles.color = Color.white;
-    //    Handles.DrawWireDisc(circlePos, Vector3.up, _radius);
-
-    //    Handles.DrawWireDisc(wanderTarget, Vector3.up, 0.2f);
-    //}
 
     void ChangeAngle()
     {
         _currentAngle += Random.Range(-_maxChange, _maxChange);
         _currentAngle = MyUtils.ClampRadians(_currentAngle);
     }
+
+
+    //private PositionMovementBehavior _movementBehavior;
+
+    ////wander based on circle in front of character
+    //[SerializeField] private float _offset = 1.0f;
+    //[SerializeField] private float _radius = 1.0f;
+    //[SerializeField] private float _maxChange = MyUtils.ToRadians(10.0f); //in radians
+    //private float _currentAngle = 0.0f; //in radians
+
+    //private void Awake()
+    //{
+    //    _movementBehavior = GetComponent<PositionMovementBehavior>();
+    //}
+
+    //void Update()
+    //{
+    //    if (_movementBehavior == null)
+    //        return;
+
+    //    ChangeAngle();
+
+    //    Vector3 circlePos = transform.position + transform.forward * _offset;
+    //    Vector3 wanderTarget = circlePos + MyUtils.DegreesToVectorXZ(_currentAngle) * _radius;
+
+    //    _movementBehavior.Destination = wanderTarget;
+    //}
+
+    //void ChangeAngle()
+    //{
+    //    _currentAngle += Random.Range(-_maxChange, _maxChange);
+    //    _currentAngle = MyUtils.ClampRadians(_currentAngle);
+    //}
 }

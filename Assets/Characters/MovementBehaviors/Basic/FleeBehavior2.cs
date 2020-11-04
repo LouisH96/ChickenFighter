2ec9c1ec2353567e735d9a5ba3602a0d26d26867
Louis Herman <louisherman@live.be>
2020-11-05 00:46:58 +0100

    added fleebehavior

---
+++ Assets/Characters/MovementBehaviors/Basic/FleeBehavior2.cs
@@ -0,0 +1,13 @@
+﻿using Assets.Characters.MovementBehaviors;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FleeBehavior2 : SeekBehavior2
+{
+    public override MovementOutput HandleMovement(MovementAgent agent)
+    {
+        _target = agent.transform.position + agent.transform.position - _target;
+        return base.HandleMovement(agent);
+    }
+}
---
+++ Assets/Characters/MovementBehaviors/Basic/MouseFleeBehavior.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFleeBehavior : FleeBehavior2
+{
+    void Update()
+    {
+        _target = MySessionUtils.Instance.GetWorldMousePosition();
+    }
+}

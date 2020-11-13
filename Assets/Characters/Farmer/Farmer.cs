1e802c2acac43c636ed63694d32daa0e194e4ad7
Louis Herman <louisherman@live.be>
2020-11-13 02:51:34 +0100

    added ui variable to farmer

--- Assets/Characters/Farmer/Farmer.cs
+++ Assets/Characters/Farmer/Farmer.cs
@@ -6,7 +6,11 @@ public class Farmer : MonoBehaviour
 {
     //---Components---
     [SerializeField] private ChickenGrab _chickenGrab = null;
+    [SerializeField] private UIManager _uiManager = null;
 
     //--- Public Member Access ---
     public ChickenGrab ChickenGrab { get { return _chickenGrab; } }
+    public UIManager UIManager { get { return _uiManager; } }
+
+
 }
